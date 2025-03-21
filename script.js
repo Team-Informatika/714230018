ecb458fcffab9271e9529ae7137bb06a47bd724d
Efendi-Sugiantoro-16 <efendisugiantoro55@gmail.com>
2025-03-21 15:46:04 +0700

    upload fil menambahkan kode tambahan

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function showAlert() {
     alert("Terima kasih telah menghubungi saya!");
 }
+
+function sendWebhook() {
+    const data = {
+        name: "Efendi Sugiantoro",
+        email: "efendisugiantoro16@gmail.com",
+        phone: "+6282332963807",
+        message: "Seseorang menekan tombol Hubungi Saya!"
+    };
+
+    fetch("https://t.if.co.id/714230018/webhook", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Webhook Response:", data);
+        alert("Pesan berhasil dikirim!");
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("Gagal mengirim pesan.");
+    });
+}
