1a262cdd2cf2a3dd0bc222bd7e3239d54406cd5d
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2025-05-15 21:57:26 +0700

    update codes

--- Belajar-Golang-1.0/logic.go
+++ Belajar-Golang-1.0/logic.go
@@ -14,3 +14,23 @@ func Or(a, b bool) bool {
 func Not(a bool) bool {
 	return !a
 }
+
+// Xor returns the logical exclusive OR of two boolean values
+func Xor(a, b bool) bool {
+	return (a || b) && !(a && b)
+}
+
+// Nand returns the logical NAND of two boolean values
+func Nand(a, b bool) bool {
+	return !(a && b)
+}
+
+// Nor returns the logical NOR of two boolean values
+func Nor(a, b bool) bool {
+	return !(a || b)
+}
+
+// Xnor returns the logical equivalence (XNOR) of two boolean values
+func Xnor(a, b bool) bool {
+	return a == b
+}
