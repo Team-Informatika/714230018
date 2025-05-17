463b2d9b96d8f67c720d2e71a069564502e9d572
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2025-05-17 20:31:33 +0700

    revision codes

---
+++ Belajar-Golang-1.0/variable_basic.go
@@ -0,0 +1,14 @@
+package main
+
+// Biodata holds personal information
+var Biodata = struct {
+	Name    string
+	Age     int
+	Address string
+	Email   string
+}{
+	Name:    "Efendi Sugiantoro",
+	Age:     25,
+	Address: "Jl. Veteran No. 21, Kediri, Jawa Timur, Indonesia",
+	Email:   "efendi.sugiantoro@example.com",
+}
