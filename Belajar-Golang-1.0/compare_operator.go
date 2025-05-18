e63416fd0f86f7b04a5ffe846b67e1316ab35615
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2025-05-18 19:24:29 +0700

    update codes

--- Belajar-Golang-1.0/compare_operator.go
+++ Belajar-Golang-1.0/compare_operator.go
@@ -1,61 +1,36 @@
 package main
 
-// EqualInts returns true if a equals b
-func EqualInts(a, b int) bool {
+// Ordered is a constraint that permits any ordered type: int, float64, etc.
+type Ordered interface {
+	~int | ~float64
+}
+
+// Equal returns true if a equals b
+func Equal[T Ordered](a, b T) bool {
 	return a == b
 }
 
-// NotEqualInts returns true if a does not equal b
-func NotEqualInts(a, b int) bool {
+// NotEqual returns true if a does not equal b
+func NotEqual[T Ordered](a, b T) bool {
 	return a != b
 }
 
-// GreaterThanInts returns true if a is greater than b
-func GreaterThanInts(a, b int) bool {
+// GreaterThan returns true if a is greater than b
+func GreaterThan[T Ordered](a, b T) bool {
 	return a > b
 }
 
-// LessThanInts returns true if a is less than b
-func LessThanInts(a, b int) bool {
+// LessThan returns true if a is less than b
+func LessThan[T Ordered](a, b T) bool {
 	return a < b
 }
 
-// GreaterThanOrEqualInts returns true if a is greater than or equal to b
-func GreaterThanOrEqualInts(a, b int) bool {
+// GreaterThanOrEqual returns true if a is greater than or equal to b
+func GreaterThanOrEqual[T Ordered](a, b T) bool {
 	return a >= b
 }
 
-// LessThanOrEqualInts returns true if a is less than or equal to b
-func LessThanOrEqualInts(a, b int) bool {
-	return a <= b
-}
-
-// EqualFloats returns true if a equals b
-func EqualFloats(a, b float64) bool {
-	return a == b
-}
-
-// NotEqualFloats returns true if a does not equal b
-func NotEqualFloats(a, b float64) bool {
-	return a != b
-}
-
-// GreaterThanFloats returns true if a is greater than b
-func GreaterThanFloats(a, b float64) bool {
-	return a > b
-}
-
-// LessThanFloats returns true if a is less than b
-func LessThanFloats(a, b float64) bool {
-	return a < b
-}
-
-// GreaterThanOrEqualFloats returns true if a is greater than or equal to b
-func GreaterThanOrEqualFloats(a, b float64) bool {
-	return a >= b
-}
-
-// LessThanOrEqualFloats returns true if a is less than or equal to b
-func LessThanOrEqualFloats(a, b float64) bool {
+// LessThanOrEqual returns true if a is less than or equal to b
+func LessThanOrEqual[T Ordered](a, b T) bool {
 	return a <= b
 }
